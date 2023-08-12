7f22369f7924c8a0b37c47e9495977d02a37a30a
Ghani M <rhani.data@gmail.com>
2023-08-12 19:31:44 +0200

    scripts

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -5,28 +5,34 @@ const modal = document.getElementById("contact-modal");
 // eslint-disable-next-line no-unused-vars
 function displayModal() {
     const modal = document.getElementById("contact-modal");
-	modal.style.display = "block";
-		if (modal.hasAttribute("aria-hidden") && main.hasAttribute("aria-hidden")) {
-			modal.setAttribute("aria-hidden", "false");
-			main.setAttribute("aria-hidden", "true");
-		}
-		const prenom = document.querySelector('#prenom');
-		prenom.focus();
+    modal.style.display = "block";
+    if (modal.hasAttribute("aria-hidden")) {
+        modal.setAttribute("aria-hidden", "false");
+        const main = document.getElementById("main");
+        if (main) {
+            main.setAttribute("aria-hidden", "true");
+        }
+        const prenom = document.querySelector('#prenom');
+        prenom.focus();
+    }
 }
 
 // Fermeture de la modal de contact 
 function closeModal() {
     const modal = document.getElementById("contact-modal");
     modal.style.display = "none";
-		if (modal.hasAttribute("aria-hidden") && main.hasAttribute("aria-hidden")) {
-			modal.setAttribute("aria-hidden", "true");
-			main.setAttribute("aria-hidden", "false");
-		}
+    if (modal.hasAttribute("aria-hidden")) {
+        modal.setAttribute("aria-hidden", "true");
+        const main = document.getElementById("main");
+        if (main) {
+            main.setAttribute("aria-hidden", "false");
+        }
+    }
 }
 
 // Fermeture avec la touche échap
 window.addEventListener("keyup", (e) => {
-	if (modal.getAttribute("aria-hidden") === "false" && e.key === "Escape") {
-		closeModal();
-	}
+    if (modal.getAttribute("aria-hidden") === "false" && e.key === "Escape") {
+        closeModal();
+    }
 });
--- scripts/utils/fonction.js
+++ scripts/utils/fonction.js
@@ -2,99 +2,95 @@ document.addEventListener('DOMContentLoaded', function() {
     // Event Listener pour le bouton "Contactez-moi"
     const contactButton = document.getElementById('contactButton');
     contactButton.addEventListener('click', function() {
-      displayModal();
+        displayModal();
     });
-  
+
     // Event Listener pour fermer le bouton modal
     const closeModalButton = document.getElementById('closeModalButton');
     closeModalButton.addEventListener('click', function() {
-      closeModal();
+        closeModal();
     });
-  
-    // Fonction pour afficher un message d'erreur
-    function displayErrorMessage(elementId, message) {
-      const errorElement = document.getElementById(elementId);
-      errorElement.textContent = message;
-      errorElement.style.color = 'red';
-    }
-  
-    // Fonction pour effacer un message d'erreur
-    function clearErrorMessage(elementId) {
-      const errorElement = document.getElementById(elementId);
-      errorElement.textContent = '';
-    }
-  
-    // Verification pour le first name
+
+    // Validation et gestion des erreurs pour le formulaire de contact
     const prenomInput = document.getElementById('prenom');
-    const prenomError = document.getElementById('prenomError');
-  
-    prenomInput.addEventListener('input', function() {
-      if (prenomInput.value.trim() === '') {
-        displayErrorMessage('prenomError', 'Veuillez saisir votre prénom.');
-      } else if (prenomInput.value.trim().length < 2) {
-        displayErrorMessage('prenomError', 'Le prénom doit comporter au moins 2 caractères.');
-      } else if (!/^[a-zA-Z]+$/.test(prenomInput.value.trim())) {
-        displayErrorMessage('prenomError', 'Le prénom ne peut pas contenir de chiffres ou de symboles.');
-      } else {
-        clearErrorMessage('prenomError');
-      }
-    });
-  
-    // Verification pour le last name
     const nomInput = document.getElementById('nom');
-    const nomError = document.getElementById('nomError');
-  
-    nomInput.addEventListener('input', function() {
-      if (nomInput.value.trim() === '') {
-        displayErrorMessage('nomError', 'Veuillez saisir votre nom.');
-      } else if (nomInput.value.trim().length < 2) {
-        displayErrorMessage('nomError', 'Le nom doit comporter au moins 2 caractères.');
-      } else if (!/^[a-zA-Z]+$/.test(nomInput.value.trim())) {
-        displayErrorMessage('nomError', 'Le nom ne peut pas contenir de chiffres ou de symboles.');
-      } else {
-        clearErrorMessage('nomError');
-      }
-    });
-  
-    // Verification pour email
     const emailInput = document.getElementById('email');
-    const emailError = document.getElementById('emailError');
-  
-    emailInput.addEventListener('input', function() {
-      const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // Regular expression to validate email address
-  
-      if (emailInput.value.trim() === '') {
-        displayErrorMessage('emailError', 'Veuillez saisir votre email.');
-      } else if (!regex.test(emailInput.value.trim())) {
-        displayErrorMessage('emailError', 'Veuillez saisir une adresse email valide.');
-      } else {
-        clearErrorMessage('emailError');
-      }
-    });
-  
-    // Verification du message
     const messageInput = document.getElementById('message');
-    const messageError = document.getElementById('messageError');
-  
-    messageInput.addEventListener('input', function() {
-      if (messageInput.value.trim() === '') {
-        displayErrorMessage('messageError', 'Veuillez saisir votre message.');
-      } else {
-        clearErrorMessage('messageError');
-      }
+
+    prenomInput.addEventListener('input', function() {
+        validateName(prenomInput, 'prenomError');
     });
-    // Supposons que totalLikes contienne le nombre total de likes
-  const totalLikes = 100; // Remplacez cela par la valeur réelle récupérée
 
-  // Récupérer l'élément span avec la classe "total-like"
-  const totalLikeElement = document.querySelector('.total-like');
+    nomInput.addEventListener('input', function() {
+        validateName(nomInput, 'nomError');
+    });
 
-  // Mettre à jour le contenu de l'élément avec le nombre total de likes
-  totalLikeElement.textContent = totalLikes;
-  });
-    // Event Listener pour le chargement de la page
-    window.addEventListener('DOMContentLoaded', function() {
-        // Code à exécuter lorsque la page est chargée
-        // Appelle une fonction par exemple :
-        // loadPhotographers();
-      });
+    emailInput.addEventListener('input', function() {
+        validateEmail(emailInput, 'emailError');
+    });
+
+    messageInput.addEventListener('input', function() {
+        validateMessage(messageInput, 'messageError');
+    });
+
+    // Affichage du nombre total de likes
+    const totalLikes = 100; // Remplacez cela par la valeur réelle récupérée
+    const totalLikeElement = document.querySelector('.total-like');
+    totalLikeElement.textContent = totalLikes;
+});
+
+// Fonction de validation du nom
+function validateName(input, errorElementId) {
+    const errorElement = document.getElementById(errorElementId);
+    const inputValue = input.value.trim();
+
+    if (inputValue === '') {
+        displayErrorMessage(errorElementId, 'Veuillez saisir votre ' + (input.id === 'prenom' ? 'prénom' : 'nom') + '.');
+    } else if (inputValue.length < 2) {
+        displayErrorMessage(errorElementId, 'Le ' + (input.id === 'prenom' ? 'prénom' : 'nom') + ' doit comporter au moins 2 caractères.');
+    } else if (!/^[a-zA-Z]+$/.test(inputValue)) {
+        displayErrorMessage(errorElementId, 'Le ' + (input.id === 'prenom' ? 'prénom' : 'nom') + ' ne peut pas contenir de chiffres ou de symboles.');
+    } else {
+        clearErrorMessage(errorElementId);
+    }
+}
+
+// Fonction de validation de l'email
+function validateEmail(input, errorElementId) {
+    const errorElement = document.getElementById(errorElementId);
+    const inputValue = input.value.trim();
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // Regular expression to validate email address
+
+    if (inputValue === '') {
+        displayErrorMessage(errorElementId, 'Veuillez saisir votre email.');
+    } else if (!regex.test(inputValue)) {
+        displayErrorMessage(errorElementId, 'Veuillez saisir une adresse email valide.');
+    } else {
+        clearErrorMessage(errorElementId);
+    }
+}
+
+// Fonction de validation du message
+function validateMessage(input, errorElementId) {
+    const errorElement = document.getElementById(errorElementId);
+    const inputValue = input.value.trim();
+
+    if (inputValue === '') {
+        displayErrorMessage(errorElementId, 'Veuillez saisir votre message.');
+    } else {
+        clearErrorMessage(errorElementId);
+    }
+}
+
+// Fonction pour afficher un message d'erreur
+function displayErrorMessage(elementId, message) {
+    const errorElement = document.getElementById(elementId);
+    errorElement.textContent = message;
+    errorElement.style.color = 'red';
+}
+
+// Fonction pour effacer un message d'erreur
+function clearErrorMessage(elementId) {
+    const errorElement = document.getElementById(elementId);
+    errorElement.textContent = '';
+}
